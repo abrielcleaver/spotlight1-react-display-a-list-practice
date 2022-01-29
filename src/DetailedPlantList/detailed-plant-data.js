7e879d0f4215c1fe638dfbea1a34edc7ad9670c9
Abriel Cleaver <abrielcleaver@gmail.com>
2022-01-28 22:59:25 -0700

    Rename array-two file to detailed-plant-data and input data for DetailedPlantList

---
+++ src/DetailedPlantList/detailed-plant-data.js
@@ -0,0 +1,37 @@
+
+export const detailedPlants = [
+  {
+        // img, name, genus, water, sun, & toxicity 
+    image: 'monstera-deliciosa.jpg',
+    name: 'Swiss Cheese plant',
+    genus: 'Monstera  Deliciosa',
+    water: 'Regular waterings during growing season every one to two weeks. During fall and winter only water occasionally. It is important for the soil to dry out slightly between waterings. ',
+    sun: 'Bright indirect sunlight, partial shade',
+    toxicity: 'Toxic to cats and dogs.',
+  },
+  {
+    image: './plant-images/radiator-plant.jpg', 
+    name: 'Radiator Plant',
+    genus: 'Peperomia',
+    water: 'It\'s better to keep this plant on the dry side so it doesn\'t need frequent watering to thrive. The surface of the soil should be dry before the next watering. ',
+    sun: 'Full or partial sunlight',
+    toxicity: 'Considered non-toxic and pet-friendly.',
+  },
+    
+  {
+    image: 'philodendron.jpg',
+    name: 'Philodendron',
+    genus: 'Philodendron',
+    water: 'Water when the top inch of soil is dried out. Reduce your waterings during winter.',
+    sun: 'Partial sunlight',
+    toxicity: 'Toxic to pets and humans if ingested.',
+  },
+  {
+    image: './plant-images/chinese-evergreen.jpg',
+    name: 'Chinese Evergreen',
+    genus: 'Aglanoema Commutatum',
+    water: 'This plant thrives best in moist soil but be mindful of overwatering. Water thoroughly and be sure to let it dry out before the next watering, but never allow the plant to dry out completely..',
+    sun: 'Full or partial sunlight',
+    toxicity: 'Toxic to cats and dogs.',
+  }
+];
