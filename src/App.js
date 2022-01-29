d7e29f8a90f94ad979ff882aaa232d93cfbc06e4
Abriel Cleaver <abrielcleaver@gmail.com>
2022-01-28 23:24:12 -0700

    Import DetailedPlantList to App. DetailedPlantList is rendering on page and showing congratulations message

--- src/App.js
+++ src/App.js
@@ -3,12 +3,18 @@ import './App.css';
 import PlantList from './PlantList/PlantList';
 import { plants } from './PlantList/plants-data';
 
+import DetailedPlantList from './DetailedPlantList/DetailedPlantList';
+import { detailedPlants } from './DetailedPlantList/detailed-plant-data';
+
 function App() {
   return (
     <div className="App">
         Render all your lists here. Pass the arrays as props.
       <div>
         <PlantList plant={plants} />
+      </div>
+      <div>
+        <DetailedPlantList dplant={detailedPlants} />
       </div>
     </div>
   );
---
+++ src/DetailedPlantList/DetailedPlantList.js
@@ -0,0 +1,10 @@
+// import DetailedPlantItem from './DetailedPlantItem';
+
+export default function DetailedPlantList() {
+  return <div className="detailed-plant-list">
+    <h2>Congratulations you rendered DetailedPlantList!</h2>
+    
+
+  </div>;
+}
+
