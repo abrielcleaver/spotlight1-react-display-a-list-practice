0aa0a01c892a1a69d674282afd8fdef7d72bf985
Abriel Cleaver <abrielcleaver@gmail.com>
2022-01-29 01:54:44 -0700

    Remove chinese evergreen from detailed-plant-data file. import philodendron and radiator plant images into data file and update image property for both. all images render properly on page

--- src/DetailedPlantList/detailed-plant-data.js
+++ src/DetailedPlantList/detailed-plant-data.js
@@ -1,4 +1,6 @@
 import monstera from './plant-images/monstera-deliciosa.jpg';
+import radiatorPlant from './plant-images/radiator-plant.jpg';
+import philodendron from './plant-images/philodendron.jpg';
 
 export const detailedPlants = [
   {
@@ -11,7 +13,7 @@ export const detailedPlants = [
     toxicity: 'Toxic to cats and dogs.',
   },
   {
-    image: './plant-images/radiator-plant.jpg', 
+    image: radiatorPlant, 
     name: 'Radiator Plant',
     genus: 'Peperomia',
     water: 'It\'s better to keep this plant on the dry side so it doesn\'t need frequent watering to thrive. The surface of the soil should be dry before the next watering. ',
@@ -20,19 +22,11 @@ export const detailedPlants = [
   },
     
   {
-    image: 'philodendron.jpg',
+    image: philodendron,
     name: 'Philodendron',
     genus: 'Philodendron',
     water: 'Water when the top inch of soil is dried out. Reduce your waterings during winter.',
     sun: 'Partial sunlight',
     toxicity: 'Toxic to pets and humans if ingested.',
   },
-  {
-    image: './plant-images/chinese-evergreen.jpg',
-    name: 'Chinese Evergreen',
-    genus: 'Aglanoema Commutatum',
-    water: 'This plant thrives best in moist soil but be mindful of overwatering. Water thoroughly and be sure to let it dry out before the next watering, but never allow the plant to dry out completely..',
-    sun: 'Full or partial sunlight',
-    toxicity: 'Toxic to cats and dogs.',
-  }
 ];
