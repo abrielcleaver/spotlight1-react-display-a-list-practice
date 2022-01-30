c404083fa1e2630e3fad987b5603ca0b98f4ead1
Abriel Cleaver <abrielcleaver@gmail.com>
2022-01-30 16:34:25 -0700

    Add data for third list of objects where one property is an array

---
+++ src/PlantsForSale/for-sale-data.js
@@ -0,0 +1,25 @@
+export const products = [
+  {
+    name: 'Pothos',
+      // image: './assets/pothos-severin-candrian-GG9Gh1_FjbM-unsplash.jpg',
+    description: [
+      'Epipremnum aureum, also known as pothos, is native to Mo\'orea',
+      'This plant is considered Tropical',
+      'The price typically ranges from $10 - $20 depending on the size'
+    ]
+  },
+  {
+    name: 'Aloe Vera',
+      // image: './assets/aloe-severin-candrian-nU9U66FMutc-unsplash.jpg',
+    description: 'Aloe vera is a succulent plant species of the genus Aloe.',
+    type: 'Succulent',
+    price: 15
+  },
+  {
+    name: 'Monstera Deliciosa',
+      // image: './assets/monstera-severin-candrian-bwsTJMnhcwE-unsplash.jpg',
+    description: 'Monstera deliciosa, also known as the "split-leaf philodendron, is native to tropical forests of southern Mexico.',
+    type: 'Tropical',
+    price: 45
+  },
+];
