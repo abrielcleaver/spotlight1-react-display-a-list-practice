962c8d9d677197ab289e1260f3d25d5ba31dd900
Abriel Cleaver <abrielcleaver@gmail.com>
2022-01-30 19:31:56 -0700

    Update h3 in DetailedPlantList and comment out some properties from DetailedPlantList so they don't render on page

--- src/DetailedPlantList/DetailedPlantItem.js
+++ src/DetailedPlantList/DetailedPlantItem.js
@@ -4,10 +4,10 @@ import './DetailedPlantList.css';
 // img, name, genus, water, sun, & toxicity 
 export default function DetailedPlantItem(props) {
   return <div className="detailed-plant-item">
-    <img src={props.image} width={300} />
     <p>{props.name}</p>
-    <p>{props.genus}</p>
-    <p>{props.water}</p>
+    <img src={props.image} width={300} />
+    {/* <p>{props.genus}</p> */}
+    {/* <p>{props.water}</p> */}
     <p>{props.sun}</p>
     <p>{props.toxicity}</p>
   </div>;
