636bb9cad359d6d62f809d36172d1e357e7f4218
Abriel Cleaver <abrielcleaver@gmail.com>
2022-01-31 16:13:10 -0700

    Update message shown for PeopleWhoLovePlantsList

--- src/PeopleWhoLovePlantsList/PeopleWhoLovePlantsList.js
+++ src/PeopleWhoLovePlantsList/PeopleWhoLovePlantsList.js
@@ -3,10 +3,8 @@ import PeopleWhoLovePlantsItem from './PeopleWhoLovePlantsItem';
 
 export default function PeopleWhoLovePlantsList(props) {
   return <div>
-    <div>
-      <h2>Congratulations, you rendered PeopleWhoLovePlantsList!</h2>
-    </div>
-    <div>
+    <h2>Here Are People Who Love Plants Just as Much as Abriel Does!</h2>
+    <div className="people-list">
       {props.person.map((person, i) => 
         <PeopleWhoLovePlantsItem key={`${person}-${i}`}
           {...person}
