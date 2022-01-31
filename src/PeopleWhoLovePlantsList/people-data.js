24b3d8f30f0d8b6aa743934e8274ead8d0b3edc9
Abriel Cleaver <abrielcleaver@gmail.com>
2022-01-31 15:47:54 -0700

    Add data and images for peopleWhoLovePlantsList

---
+++ src/PeopleWhoLovePlantsList/people-data.js
@@ -0,0 +1,47 @@
+import plantkween from './people-images/plant-kween.png';
+import mel from './people-images/mel-douglas.png';
+import amber from './people-images/amber.png';
+
+export const people = [
+  {
+    name: 'Christopher Griffin aka Plant Kween',
+    image: plantkween,
+    pronouns: 'she/her',
+    description: {
+      firstName: 'Christopher',
+      location: 'Brooklyn-based,',
+      identity: 'Black queer non-binary kween and LGBTQIA+ activist',
+      passion: 'community building through plants!',
+      instagram: ' @plantkween',
+    }
+  },
+  {
+    name: 'Mel Douglas',
+    image: mel,
+    pronouns: 'she/her',
+    description: {
+      firstName: 'Mel',
+      location: 'LA-based, Brooklyn raised',
+      identity: 'Yoga teacher, Black lesbian activist, and well-being advocate',
+      passion: 'the founder of Black Women’s Yoga Collective (@blackwomensyogaco), a Health & wellness organization dedicated to increasing access to wellness for Black and brown folks.',
+      instagram: ' @meldouglasyoga',
+    }
+  },
+  {
+    name: 'Amber Paige',
+    image: amber,
+    pronouns: 'she/her',
+    description: {
+      firstName: 'Amber',
+      location: 'North Carolina based',
+      identity: 'gardener in zone 8a',
+      passion: 'the founder of Black Girls Gardening (@blackgirlsgardening), a community to inspire Black women and girls to plant and garden!',
+      instagram: ' @amberxpaigee',
+    }
+  }
+];
+
+
+
+
+
